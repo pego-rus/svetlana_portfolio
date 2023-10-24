8055bd0b4e5f941f4b9500d57a2b35c0df9085c7
Pervov Egor <pego_rus@mail.ru>
2023-10-24 14:34:48 +0300

    scrollIntoView test

--- src/components/screens/home/mainScreen/Header.jsx
+++ src/components/screens/home/mainScreen/Header.jsx
@@ -1,14 +1,27 @@
 import styles from './mainScreen.module.css'
+import { useRef } from 'react';
 
 const Header = () => {
+    const ref1 = useRef('')
+    const ref2 = useRef(null)
+    const ref3 = useRef(null)
+    const handleClick1 = () => {
+        ref1.current.scrollIntoView();
+    }
+    const handleClick2 = () => {
+        ref2.current.scrollIntoView();
+    }
+    const handleClick3 = () => {
+        ref3.current.scrollIntoView();
+    }
     return (
     <>
     <header>
         <nav className={styles.navBar}>
             <ul>
-                <li>Портфолио</li>
-                <li>Примеры работ</li>
-                <li>Контакты</li>
+                <li onClick={handleClick1}>Портфолио</li>
+                <li onClick={handleClick2}>Примеры работ</li>
+                <li onClick={handleClick3}>Контакты</li>
             </ul>
         </nav>
     </header>
