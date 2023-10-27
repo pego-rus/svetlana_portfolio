ce7f8edd30db78b3f72c73a9095b0e67b07fe190
Pervov Egor <pego_rus@mail.ru>
2023-10-27 17:28:00 +0300

    testing scroll 2

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import MainScreen from './components/screens/home/mainScreen/MainScreen'
 import PortfolioPage from './components/screens/home/portfolioPage/PortfolioPage'
 import Footer from './components/screens/home/footer/Footer'
 import './assets/styles/global.css'
+import { useRef } from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <MainScreen />
-    <PortfolioPage />
+      const ref = useRef(null)
+      const ref2 = useRef(null)
+      const ref3 = useRef(null)
+    <MainScreen ref ={ ref }/>
+    <PortfolioPage ref ={ ref }/>
     <Footer />
   </React.StrictMode>,
 )
