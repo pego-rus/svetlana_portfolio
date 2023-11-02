74d20f3baa4125b0f406dee83a7683df0d9edf4c
pego-rus <pego_rus@mail.ru>
2023-11-02 20:47:45 +0300

    some bugs fixed

--- src/components/screens/home/contactForm/ContactMe.jsx
+++ src/components/screens/home/contactForm/ContactMe.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react';
 import styles from './contactMe.module.css'
 
+const clearData = {
+    name: '',
+    email: '',
+    phone: '',
+}
+
 const ContactMe = () => {
 
-    const [input, setInput] = useState({});
+    const [input, setInput] = useState(clearData);
 
     const handleChange = (event) => {
         let name = event.target.dataset.text;
@@ -14,15 +20,16 @@ const ContactMe = () => {
         const newRequest = JSON.stringify(input);
         console.log(newRequest);
         el.preventDefault();
+        setInput(clearData)
     };
 
     return (
         <div id='contacts' className={styles.formDiv}>
             <form className={styles.getRequest}>
                 <h2>Форма для связи</h2>
-                <input type="text" data-text='name' placeholder='Name' onChange={handleChange} />
-                <input type="text" data-text='email' placeholder='Электронная почта' onChange={handleChange} />
-                <input type="text" data-text='phone' placeholder='Номер телефона' onChange={handleChange} />
+                <input type="text" data-text='name' value={input.name} placeholder='Name' onChange={handleChange} />
+                <input type="text" data-text='email' value={input.email} placeholder='Электронная почта' onChange={handleChange} />
+                <input type="text" data-text='phone' value={input.phone} placeholder='Номер телефона' onChange={handleChange} />
                 <button onClick = {clickHandler}>Отправить запрос на сотрудничество</button>
             </form>
         </div>
